12f6f297a2abaccfce6c49a46a6c513fb9f796d7
Leyla <lkursat@stanford.edu>
2019-12-04 15:17:35 -0800

    feature namability ready

---
+++ experiments/norming/feature_namability/pilot/list1/public/js/list2.js
@@ -0,0 +1,150 @@
+  var items_target = _.shuffle([
+{
+"label": "bag_canvas_green",
+"item": ["bag"]
+},
+{
+"label": "bag_paper_original",
+"item": ["bag"]
+},
+{
+"label": "bag_plastic_blue",
+"item": ["bag"]
+},
+{
+"label": "ball_metal_original",
+"item": ["ball"]
+},
+{
+"label": "ball_plastic_blue",
+"item": ["ball"]
+},
+{
+"label": "boot_leather_green",
+"item": ["boot"]
+},
+{
+"label": "boot_rubber_brown",
+"item": ["boot"]
+},
+{
+"label": "bottle_glass_original",
+"item": ["bottle"]
+},
+{
+"label": "bottle_plastic_green",
+"item": ["bottle"]
+},
+{
+"label": "bowl_metal_blue",
+"item": ["bowl"]
+},
+{
+"label": "bowl_wood_purple",
+"item": ["bowl"]
+},
+{
+"label": "box_cardboard_green",
+"item": ["box"]
+},
+{
+"label": "box_wood_blue",
+"item": ["box"]
+},
+{
+"label": "chair_metal_original",
+"item": ["chair"]
+},
+{
+"label": "chair_plastic_green",
+"item": ["chair"]
+},
+{
+"label": "chair_wood_pink",
+"item": ["chair"]
+},
+{
+"label": "cup_metal_blue",
+"item": ["cup"]
+},
+{
+"label": "cup_plastic_original",
+"item": ["cup"]
+},
+{
+"label": "door_steel_yellow",
+"item": ["door"]
+},
+{
+"label": "door_wood_red",
+"item": ["door"]
+},
+{
+"label": "jacket_denim_original",
+"item": ["jacket"]
+},
+{
+"label": "jacket_leather_green",
+"item": ["jacket"]
+},
+{
+"label": "pitcher_glass_original",
+"item": ["pitcher"]
+},
+{
+"label": "pitcher_metal_orange",
+"item": ["pitcher"]
+},
+{
+"label": "pitcher_plastic_green",
+"item": ["pitcher"]
+},
+{
+"label": "plate_metal_green",
+"item": ["plate"]
+},
+{
+"label": "plate_paper_pink",
+"item": ["plate"]
+},
+{
+"label": "plate_plastic_original",
+"item": ["plate"]
+},
+{
+"label": "ruler_metal_original",
+"item": ["ruler"]
+},
+{
+"label": "ruler_wood_green",
+"item": ["ruler"]
+},
+{
+"label": "spoon_metal_blue",
+"item": ["spoon"]
+},
+{
+"label": "spoon_plastic_original",
+"item": ["spoon"]
+},
+{
+"label": "spoon_wood_green",
+"item": ["spoon"]
+},
+{
+"label": "table_metal_blue",
+"item": ["table"]
+},
+{
+"label": "table_wood_original",
+"item": ["table"]
+},
+{
+"label": "vase_glass_green",
+"item": ["vase"]
+},
+{
+"label": "vase_wood_original",
+"item": ["vase"]
+},
+]);

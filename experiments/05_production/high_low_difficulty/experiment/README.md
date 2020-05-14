61a8d94491dae6672a0ae3e497eae3f10004db5a
Leyla <lkursat@stanford.edu>
2020-05-13 21:38:26 -0700

    production exp done

---
+++ experiments/05_production/high_low_difficulty/experiment/README.md
@@ -0,0 +1,19 @@
+# Experiments
+
+This directory contains all the experiments reported in the paper.
+
+## Production experiments (Exp. 1 - 3)
+
+The main experiments reported in the paper are available in the directories `production_*`. Exps. 1 and 3 were run within the same experiment. To run the experiments locally on your machine
+
+1. `cd production`
+
+2. `npm install` to locally install dependencies
+
+3. `node app.js <experimentname>` (i.e. `production_exp2` or `production_exp1_exp3`)
+
+4. go to `localhost:8889/<experimentname>/experiment/index.html` in your browser
+
+## Typicality norming experiments
+
+The series of norming experiments that were run to elicit typicality norms are available in the directories `norming_expX(_TYPE)`. Any of these experiments can be run by opening each experiment's respective norming.html file in a browser.
---
+++ experiments/05_production/high_low_difficulty/experiment/stimuli/objectSet.js
@@ -0,0 +1,29 @@
+var conds = [
+	{label: 'chair_metal_purple', condition: 'high_difficulty', color: 'purple', material: 'metal', item: "chair", url: 'stimuli/chair_metal_purple.png', width: 300, height: 300},
+	{label: 'table_metal_blue', condition: 'high_difficulty', color: 'blue', material: 'metal', item: "table", url: 'stimuli/table_metal_blue.png', width: 300, height: 300},
+	{label: 'table_metal_green', condition: 'high_difficulty', color: 'green', material: 'metal', item: "table", url: 'stimuli/table_metal_green.png', width: 300, height: 300},
+	{label: 'table_metal_silver_original', condition: 'high_difficulty', color: 'silver', material: 'metal', item: "table", url: 'stimuli/table_metal_silver_original.png', width: 300, height: 300},
+	{label: 'boot_leather_green', condition: 'high_difficulty', color: 'green', material: 'leather', item: "boot", url: 'stimuli/boot_leather_green.png', width: 300, height: 300},
+	{label: 'chair_metal_green', condition: 'high_difficulty', color: 'green', material: 'metal', item: "chair", url: 'stimuli/chair_metal_green.png', width: 300, height: 300},
+	{label: 'plate_paper_blue', condition: 'high_difficulty', color: 'blue', material: 'paper', item: "plate", url: 'stimuli/plate_paper_blue.png', width: 300, height: 300},
+	{label: 'spoon_wood_green', condition: 'high_difficulty', color: 'green', material: 'wood', item: "spoon", url: 'stimuli/spoon_wood_green.png', width: 300, height: 300},
+    {label: 'plate_plastic_blue_original', condition: 'low_difficulty', color: 'blue', material: 'plastic', item: 'plate', url: 'stimuli/plate_plastic_blue_original.png', width: 300, height: 300},
+    {label: 'cup_plastic_green', condition: 'low_difficulty', color: 'green', material: 'plastic', item: 'cup', url: 'stimuli/cup_plastic_green.png', width: 300, height: 300},
+    {label: 'jacket_denim_purple', condition: 'low_difficulty', color: 'purple', material: 'denim', item: 'jacket', url: 'stimuli/jacket_denim_purple.png', width: 300, height: 300},
+    {label: 'pitcher_metal_blue', condition: 'low_difficulty', color: 'blue', material: 'metal', item: 'pitcher', url: 'stimuli/pitcher_metal_blue.png', width: 300, height: 300},
+    {label: 'table_metal_blue', condition: 'low_difficulty', color: 'blue', material: 'metal', item: 'table', url: 'stimuli/table_metal_blue.png', width: 300, height: 300},
+    {label: 'bottle_glass_green', condition: 'low_difficulty', color: 'green', material: 'glass', item: 'bottle', url: 'stimuli/bottle_glass_green.png', width: 300, height: 300},
+]
+
+// missing:
+// {label: 'chair_plastic_blue_original', condition: 'low_difficulty', color: 'blue', material: 'plastic', item: 'chair', url: 'stimuli/chair_plastic_blue_original.png', width: 300, height: 300},
+// {label: 'table_wood_brown_original', condition: 'low_difficulty', color: 'brown', material: 'wood', item: 'table', url: 'stimuli/table_wood_brown_original.png', width: 300, height: 300},
+
+
+var targetList = [
+	conds
+]
+
+module.exports = targetList;
+
+
